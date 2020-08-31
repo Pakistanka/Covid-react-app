daed41119629b0ddffafa0a1fe40852d42b75d35
mac <mac@macs-MacBook-Pro.local>
2020-08-31 11:00:13 +0200

    add api feature

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React from 'react';
 // multi import
 import {Cards, Chart, CountryPicker} from './components/index';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 
 class App extends React.Component {
+
+    // use 'async' if your function async
+    async componentDidMount() {
+        // use 'await' for async functions
+        const data = await fetchData();
+
+        console.log(data);
+    }
+
     render() {
         return (
             <div className={styles.container}>
---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+// This file for creating api calls
+//  We will write functions to fetch data what we need
+
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+// async function
+export const fetchData = async () => {
+    // async return answer from api
+    try {
+        // get special data from api
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        // destructure data
+        // если key и value имею одинаковое имя, то можно не писать
+        // key: value, а использовать одно значение
+        const modifiedData = { confirmed, recovered, deaths, lastUpdate }
+
+        return modifiedData;
+    } catch (error) {
+
+    }
+
+}
+
